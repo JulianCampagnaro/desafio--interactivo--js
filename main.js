6c6aa3acff2cf3d6869148269dbb4f1c35efdd74
Julian Campagnaro <jcampagnaro123@gmail.com>
2023-02-05 10:00:59 -0300

    finalización del código

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let max=100;
 let min=1;
 let numeroOculto= Math.random ()* (max-min) + min; /* Con esta ecuación generamos el número
 random que se debe adivinar */
+numeroOculto=parseInt(numeroOculto); /* convertimos en entero el valor en caso
+de que sea un flotante */
 
 /* Pedimos al usuario que ingrese */
 let usuarioIngresado= prompt("Ingrese su nombre de usuario:");
@@ -15,11 +17,20 @@ let pswIngresada=prompt ("Ingrese su contraseña:");
 /* Validamos */
 if (usuarioIngresado===usuarioAutorizado && pswIngresada===pswAutorizada){
     alert ("Bienvenido " + usuarioIngresado);
-
-
-
-
-
-
-
+    while (true){
+        let numUsuario=prompt("Ingresa un número para adivinar el mágico: ");
+        if (numUsuario===numeroOculto){
+            alert ("Felicitaciones " + usuarioIngresado + " has adivinado el número mágico");
+            break;
+        }else if (numUsuario==0){
+            alert ("Decidiste abandonar el juego");
+            break;
+        }else if(numUsuario < numeroOculto){
+            alert ("El número ingresado es menor al mágico, seguí intentando");
+        }else if(numUsuario > numeroOculto){
+            alert ("El número ingresado es mayor al mágico, seguí intentando")
+        }
+    }
+} else {
+    alert ("Usuario y/o contraseña incorrecta");
 }
